fad690b08c12b1de33a638d5ec62cae4754e8f0e
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2025-12-28 21:27:47 +0100

    Update st_trans.py

--- st_trans.py
+++ st_trans.py
@@ -719,7 +719,7 @@ def render_sqs_module():
             with st.expander("How to cite", icon="📚", expanded=True):
                 st.markdown("""
                  Please cite the following sources
-                 - **SimplySQS interface (PREPRINT, arXiv)** - [LEBEDA, Miroslav, et al. Simplifying Generation of Special Quasirandom Structures with ATAT Using Interactive Online Interface-SimplySQS. arXiv preprint arXiv:2510.18020, 2025](https://arxiv.org/abs/2510.18020).
+                 - **SimplySQS interface (PREPRINT, arXiv)** - [LEBEDA, Miroslav, et al. SimplySQS: An Automated and Reproducible Workflow for Special Quasirandom Structure Generation with ATAT. arXiv preprint arXiv:2510.18020, 2025](https://arxiv.org/abs/2510.18020).
                  - **ATAT mcsqs method** - [VAN DE WALLE, Axel, et al. Efficient stochastic generation of special quasirandom structures. Calphad, 2013](https://www.sciencedirect.com/science/article/pii/S0364591613000540?casa_token=i1iog7eW3lQAAAAA:wxlTn-9Twj38XFx1lMfSazPb6r0JrDV7NPxeums5-2qFXHWItT2ZVu9E-IfuBjRsr7f1BEzcSw).
                  - **ATAT** - [VAN DE WALLE, Axel; ASTA, Mark; CEDER, Gerbrand. The alloy theoretic automated toolkit: A user guide. Calphad, 2002](https://www.sciencedirect.com/science/article/abs/pii/S0364591602800062).
             """)
