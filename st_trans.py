64fc4fd4c03e325b30086af6b32c11fc9e264209
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2026-03-19 21:56:56 +0100

    Update st_trans.py

--- st_trans.py
+++ st_trans.py
@@ -2038,7 +2038,7 @@ def render_sqs_module():
         st.info("""
             Special Quasirandom Structures (SQS) approximate random alloys by matching the correlation functions 
             of a truly random alloy in a finite supercell.
-            See also our corresponding **[preprint (arXiv)](https://arxiv.org/abs/2510.18020)** for **SimplySQS**.
+            See also our corresponding **[publication](https://doi.org/10.1016/j.jocs.2026.102846)** for **SimplySQS**.
         """)
         intro_text()
 
